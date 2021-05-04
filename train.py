2e4830c7b06c2da7a107c53d896caf04168a1f4c
Valerio Marsocci <49810041+VMarsocci@users.noreply.github.com>
2021-05-04 16:45:51 +0200

    yaml and tracking metrics added

--- train.py
+++ train.py
@@ -8,6 +8,10 @@ from torchvision import transforms as T
 import numpy as np
 import json
 from datetime import datetime
+from PIL import ImageFilter
+import pathlib
+import yaml
+import os
 
 from tqdm import tqdm, trange
 
@@ -19,31 +23,71 @@ from cp import *
 from sce import *
 from eval.metrics import *
 
-pretrained = 'imagenet'
-CHECKPOINTS = '../U-Net_08_03/imagenet_weights'
-batch_size = 128
-niter = 20
-class_num = 6
-learning_rate = 0.0001 * 3
-beta1 = 0.5
+class GaussianBlur(object):
+    def __init__(self, sigma=[.1, 2.]):
+        self.sigma = sigma
+
+    def __call__(self, x):
+        sigma = np.random.uniform(self.sigma[0], self.sigma[1])
+        x = x.filter(ImageFilter.GaussianBlur(radius=sigma))
+        return x
+
+    def __str__(self):
+        str_transforms = f"GaussianBlur(sigma={self.sigma})"
+        return str_transforms
+
+project_root = "."
+config_name = "MARE"
+default_dst_dir = str(pathlib.Path(project_root) / "experiments")
+
+exp_directory = pathlib.Path(default_dst_dir) / config_name
+os.makedirs(exp_directory, exist_ok=True)
+
+# Load the configuration params of the experiment
+full_config_path = pathlib.Path(project_root) / (config_name + ".yaml")
+print(f"Loading experiment {full_config_path}")
+with open(full_config_path, "r") as f:
+    exp_config = yaml.load(f, Loader=yaml.SafeLoader)
+exp_dir = exp_directory
+
+print(f"Logs and/or checkpoints will be stored on {exp_directory}")
+
+pretrained = exp_config['model']['pretraining_arch']
+CHECKPOINTS = exp_config['model']['checkpoints_path']
+
+batch_size = exp_config['data']['train']['batch_size']
+niter = exp_config['optim']['num_epochs']
+class_num = exp_config['model']['num_classes']
+
+learning_rate = exp_config['optim']['lr']
+end_learning_rate = exp_config['optim']['end_lr']
+betas = exp_config['optim']['beta']
+weight_decay = exp_config['optim']['weight_decay']
+
 cuda = True
-num_workers = 1
-size_h = 256
-size_w = 256
+num_workers = 4
+num_GPU = 1
+torch.cuda.set_device(0)
+
+size_h = exp_config['data']['size'][0]
+size_w = exp_config['data']['size'][1]
 flip = 0
-band = 3
-train_path = '../Vaihingen/dataset/training/'
-val_path = '../Vaihingen/dataset/validation/'
-test_path = '../Vaihingen/test'
+band = exp_config['data']['channels']
+
+train_path = exp_config['data']['train']['path']
+val_path = exp_config['data']['val']['path']
+test_path = exp_config['data']['test']['path']
 
 encoder, pretrained, _ = pretrain_strategy(pretrained, CHECKPOINTS)
 net = MAResUNet(band, class_num, base_model= encoder)
-net.name = datetime.today().strftime('%Y_%m_%d') + '_' + pretrained
+net.name = exp_config['general']['test_id'] + '_' + exp_config['general']['test_type']
 
-out_file = './checkpoint/' + net.name
-num_GPU = 1
-index = 6000
-torch.cuda.set_device(0)
+out_file = str(exp_dir) + '/' + net.name
+
+index = exp_config['data']['train']['samples']
+val_index = exp_config['data']['val']['samples']
+test_index = exp_config['data']['test']['samples']
+
 
 try:
     import os
@@ -52,6 +96,7 @@ try:
       print('Checkpoints successfully loaded!')
     else:
       os.makedirs(out_file)
+      print('No checkpoints founds -> Directory created successfully')
 except OSError:
     pass
 
@@ -62,20 +107,20 @@ cudnn.benchmark = True
 
 train_trans = T.Compose([
   T.ToPILImage(),
-  T.RandomApply([T.ColorJitter((0.4, 1.5), (0.2, 2), (0.2, 2), (-0.3, 0.3))], p=0.5),
-  T.RandomGrayscale(p = 0.3),
-  T.GaussianBlur(3),
+  T.RandomApply([T.ColorJitter(*exp_config['data']['train']['transforms']['cjitter'])], 
+                 p= exp_config['data']['train']['transforms']['cjitter_p']),
+  T.RandomGrayscale(p = exp_config['data']['train']['transforms']['gray_p']),
+  T.RandomApply([GaussianBlur(exp_config['data']['train']['transforms']['gaussian_blur'])], 
+                 p = exp_config['data']['train']['transforms']['gaussian_blur_p']),
   T.ToTensor(),
-  T.Normalize([0.3206467284225958, 0.3230442137290628, 0.4726621112943237], 
-  [0.1507140017014572, 0.15590631427820206, 0.21559699547025785])
+  T.Normalize(*exp_config['data']['train']['transforms']['normalization'])
 ])
 
 print(train_trans)
 
 val_trans = T.Compose([
   T.ToTensor(),
-  T.Normalize([0.3156378902694481, 0.31905504344848745, 0.4772593356486166],
-  [0.14555519829539584, 0.15285215419466583, 0.21367692564739332])
+  T.Normalize(*exp_config['data']['val']['transforms']['normalization'])
 ])
 
 train_dataset_ = train_dataset(train_path, size_w, size_h, 
@@ -100,35 +145,72 @@ if num_GPU > 1:
 
 
 ###########   LOSS & OPTIMIZER   ##########
-# criterion = nn.CrossEntropyLoss(ignore_index=255)
-criterion = SoftCrossEntropyLoss(smooth_factor= 0.1, n_classes = class_num, ignore_index=255)
-optimizer = torch.optim.AdamW(net.parameters(), lr=learning_rate)
-early_stopping = EarlyStopping(patience=10, verbose=True)
+if exp_config['model']['loss'] == 'crossentropy':
+    criterion = nn.CrossEntropyLoss(ignore_index=255)
+elif exp_config['model']['loss'] == 'softcrossentropy':
+    criterion = SoftCrossEntropyLoss(smooth_factor= 0.1, n_classes = class_num, ignore_index=255)
+else:
+    print('Loss not implemented yet. Cross Entropy selected by default')
+    criterion = nn.CrossEntropyLoss(ignore_index=255)
+
+if exp_config['optim']['optim_type'] == 'adamw':
+    optimizer = torch.optim.AdamW(net.parameters(), 
+                                  lr=learning_rate, 
+                                  betas = betas,
+                                  weight_decay=weight_decay)
+elif exp_config['optim']['optim_type'] == 'adam':
+    optimizer = torch.optim.Adam(net.parameters(), 
+                                  lr=learning_rate, 
+                                  betas = betas,
+                                  weight_decay=weight_decay)
+else:
+    print('Optim not implemented yet. Adam selected by default')
+
+
+early_stopping = EarlyStopping(patience=exp_config['optim']['patience'], 
+                               verbose=True)
 
 if __name__ == '__main__':
     start = time.time()
     track_metrics = {}
+    track_lr = []
+    track_loss = []
+    track_OA = []
+    track_acc = []
+    track_miou = []
+    track_f1 = []
     net.train()
     lr_adjust = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 10, eta_min=learning_rate * 0.01, last_epoch=-1)
     for epoch in range(1, niter + 1):
-        for iter_num in trange(index//batch_size, desc='train, epoch:%s' % epoch):
-            train_iter = train_dataset_.data_iter_index(index=index)
-            for initial_image, semantic_image in train_iter:
-                initial_image = initial_image.cuda()
-                semantic_image = semantic_image.cuda()
+        sum_loss = 0
+        train_iter = train_dataset_.data_iter_index(index=index)
+        for param_group in optimizer.param_groups:
+            track_lr.append(param_group['lr'])
+            print("Epoch: %s" % epoch, " - Learning rate: ", param_group['lr'])
 
-                semantic_image_pred = net(initial_image)
+        for initial_image, semantic_image in tqdm(train_iter, desc='train'):
+            initial_image = initial_image.cuda()
+            semantic_image = semantic_image.cuda()
 
-                loss = criterion(semantic_image_pred, semantic_image.long())
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+            semantic_image_pred = net(initial_image)
+
+            loss = criterion(semantic_image_pred, semantic_image.long())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            
+            sum_loss += loss.detach().cpu().numpy()*initial_image.shape[0]
+        
+        epoch_loss = sum_loss/index
+        track_loss.append(epoch_loss)
+        print("Training loss: ", epoch_loss)
+
         lr_adjust.step()
+        
 
         with torch.no_grad():
             net.eval()
-            # val_iter = val_dataset_.data_iter_index(index=index)
-            val_iter = val_dataset_.data_iter()
+            val_iter = val_dataset_.data_iter_index(index=val_index)
             metrics = []
 
             for initial_image, semantic_image in tqdm(val_iter, desc='val'):
@@ -147,29 +229,32 @@ if __name__ == '__main__':
         metrics.append(metric)
 
         m_arr = np.mean(np.array(metrics), axis = 0)
-        track_metrics[epoch] = list(m_arr)
+        # track_metrics[epoch] = list(m_arr)
+        track_OA.append(m_arr[0])
+        track_acc.append(m_arr[1])
+        track_miou.append(m_arr[2])
+        track_f1.append(m_arr[3])
+        print("Val metrics:")
         print("OA:", m_arr[0], '\tACC_per_Class:', m_arr[1], "\tmIoU:", m_arr[2], "\tF1:", m_arr[3])
-
         net.train()
 
         early_stopping(1 - m_arr[2], net, '%s/' % out_file + 'netG.pth')
 
         if early_stopping.early_stop:
             break
+            
+        print()
 
     end = time.time()
 
-    with open(out_file+'/val_metrics.json', 'w') as outfile:
-      json.dump(track_metrics, outfile)
-
     print('Training completed. Program processed ', end - start, 's, ', (end - start)/60, 'min, ', (end - start)/3600, 'h')
 
-    test_datatset_ = train_dataset(test_path, time_series=band, transform =val_trans)
+    test_datatset_ = train_dataset(test_path, time_series=band, transform = val_trans)
     start = time.time()
-    test_iter = test_datatset_.data_iter()
-    # test_iter = test_datatset_.data_iter_index(index = index)
+    test_iter = test_datatset_.data_iter_index(index = test_index)
     if os.path.exists('%s/' % out_file + 'netG.pth'):
         net.load_state_dict(torch.load('%s/' % out_file + 'netG.pth'))
+        print("Checkpoints correctly loaded: ", out_file)
 
     net.eval()
 
@@ -191,4 +276,14 @@ if __name__ == '__main__':
     end = time.time()
     print('Test completed. Program processed ', end - start, 's, ', (end - start)/60, 'min, ', (end - start)/3600, 'h')
     print("OA:", m_arr[0], '\tACC_per_Class:', m_arr[1], "\tmIoU:", m_arr[2], "\tF1:", m_arr[3])
-    # print(metric)
+    
+    track_metrics['train_loss'] = track_loss
+    track_metrics['train_val_OA'] = track_OA
+    track_metrics['train_val_acc_per_class'] = track_acc
+    track_metrics['train_val_miou'] = track_miou
+    track_metrics['train_val_f1'] = track_f1
+    track_metrics['lr'] = track_lr
+    track_metrics['test'] = m_arr
+
+    with open(out_file+'/track_metrics.json', 'w') as outfile:
+      json.dump(track_metrics, outfile)
